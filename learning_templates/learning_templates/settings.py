5450a84dd02b16b24ecfc83c14caf532049515ce
DistrictNo9 <73796289+DistrictNo9@users.noreply.github.com>
2021-01-16 11:41:13 +0100

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '3gr11hc0d+v%h)i5$1xxe)@m)r9jx9unpdxzsmb0b&t(@zujq7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
